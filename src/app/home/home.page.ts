1c56ae468ec56adfac147caab88f6d691391ce16
madafakaoficial <sebastiangodoy45@hotmail.com>
2019-10-23 14:31:10 -0300

    version v1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -13,38 +13,23 @@ export class HomePage {
       audio: 'assets/sonidos/img1.mp3'
 
     }
-    ,{
+    , {
       nombre: 'img2',
       imagen: 'assets/imagenes/img2.png',
       audio: 'assets/sonidos/img3.mp3'
 
-    } ,{
-      nombre: 'img3',
-      imagen: 'assets/imagenes/img3.png',
-      audio: 'assets/sonidos/img2.mp3'
-
-    } ,{
-      nombre: 'img4',
-      imagen: 'assets/imagenes/img4.png',
-      audio: 'assets/sonidos/img4.mp3'
-
-    },{
-      nombre: 'img5',
-      imagen: 'assets/imagenes/img5.png',
-      audio: 'assets/sonidos/img5.mp3'
-
     }
-  ]
-    reproducirSonido(instrumento){
-      //console.log(instrumento)
-      let sonido = new Audio();
+  ];
+    reproducirSonido(instrumento) {
+      // console.log(instrumento)
+      const sonido = new Audio();
 
       sonido.src = instrumento.audio;
-      
+
       sonido.load();
-      
+
       sonido.play();
-      
+
     }
 
 }
